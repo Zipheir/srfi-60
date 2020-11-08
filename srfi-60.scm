718732811190b3234befb71bcb47c2e54cbbb0e5
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-11-08 08:41:47 -0800

    Replaced srfi-60 implementation with that from bitwise-utils
    On recommmendation of Kon Lovett, the author of bitwise-utils

--- srfi-60.scm
+++ srfi-60.scm
@@ -18,167 +18,39 @@
 ;each case.
 
 ;;; chicken-specific module
-(module
-srfi-60
-(logand bitwise-and logior bitwise-ior logxor bitwise-xor lognot
-bitwise-not bitwise-if bitwise-merge logtest any-bits-set? logcount
-bit-count integer-length log2-binary-factors first-set-bit logbit?
-bit-set? copy-bit bit-field copy-bit-field ash arithmetic-shift
-rotate-bit-field reverse-bit-field integer->list list->integer)
 
-(import scheme)
-(import (chicken bitwise))
+(module srfi-60
 
-(define logical:boole-xor
- '#(#(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15)
-    #(1 0 3 2 5 4 7 6 9 8 11 10 13 12 15 14)
-    #(2 3 0 1 6 7 4 5 10 11 8 9 14 15 12 13)
-    #(3 2 1 0 7 6 5 4 11 10 9 8 15 14 13 12)
-    #(4 5 6 7 0 1 2 3 12 13 14 15 8 9 10 11)
-    #(5 4 7 6 1 0 3 2 13 12 15 14 9 8 11 10)
-    #(6 7 4 5 2 3 0 1 14 15 12 13 10 11 8 9)
-    #(7 6 5 4 3 2 1 0 15 14 13 12 11 10 9 8)
-    #(8 9 10 11 12 13 14 15 0 1 2 3 4 5 6 7)
-    #(9 8 11 10 13 12 15 14 1 0 3 2 5 4 7 6)
-    #(10 11 8 9 14 15 12 13 2 3 0 1 6 7 4 5)
-    #(11 10 9 8 15 14 13 12 3 2 1 0 7 6 5 4)
-    #(12 13 14 15 8 9 10 11 4 5 6 7 0 1 2 3)
-    #(13 12 15 14 9 8 11 10 5 4 7 6 1 0 3 2)
-    #(14 15 12 13 10 11 8 9 6 7 4 5 2 3 0 1)
-    #(15 14 13 12 11 10 9 8 7 6 5 4 3 2 1 0)))
+(;exports
+  logand bitwise-and logior bitwise-ior logxor bitwise-xor lognot
+  bitwise-not bitwise-if bitwise-merge logtest any-bits-set? logcount
+  bit-count integer-length log2-binary-factors first-set-bit logbit?
+  bit-set? copy-bit bit-field copy-bit-field ash arithmetic-shift
+  rotate-bit-field reverse-bit-field integer->list list->integer)
 
-(define logical:boole-and
- '#(#(0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0)
-    #(0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1)
-    #(0 0 2 2 0 0 2 2 0 0 2 2 0 0 2 2)
-    #(0 1 2 3 0 1 2 3 0 1 2 3 0 1 2 3)
-    #(0 0 0 0 4 4 4 4 0 0 0 0 4 4 4 4)
-    #(0 1 0 1 4 5 4 5 0 1 0 1 4 5 4 5)
-    #(0 0 2 2 4 4 6 6 0 0 2 2 4 4 6 6)
-    #(0 1 2 3 4 5 6 7 0 1 2 3 4 5 6 7)
-    #(0 0 0 0 0 0 0 0 8 8 8 8 8 8 8 8)
-    #(0 1 0 1 0 1 0 1 8 9 8 9 8 9 8 9)
-    #(0 0 2 2 0 0 2 2 8 8 10 10 8 8 10 10)
-    #(0 1 2 3 0 1 2 3 8 9 10 11 8 9 10 11)
-    #(0 0 0 0 4 4 4 4 8 8 8 8 12 12 12 12)
-    #(0 1 0 1 4 5 4 5 8 9 8 9 12 13 12 13)
-    #(0 0 2 2 4 4 6 6 8 8 10 10 12 12 14 14)
-    #(0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15)))
+(import scheme
+  (chicken base)
+  (chicken bitwise)
+  bitwise-utils)
 
-(define (logical:ash-4 x)
-  (if (negative? x)
-      (+ -1 (quotient (+ 1 x) 16))
-      (quotient x 16)))
+(define logand              bitwise-and)
+(define logior              bitwise-ior)
+(define logxor              bitwise-xor)
+(define lognot              bitwise-not)
+(define logtest             bitwise-any?)
+(define logcount            bitwise-count)
+(define logbit?             bitwise-nth?)
+(define ash                 arithmetic-shift)
+(define bit-field           bitwise-field)
+(define copy-bit-field      bitwise-field-copy)
+(define rotate-bit-field    bitwise-field-rotate)
+(define reverse-bit-field   bitwise-field-reverse)
+(define log2-binary-factors bitwise-first-set)
+(define (copy-bit k n b)    (bitwise-set-nth n k b))
+(define bitwise-if          bitwise-merge)
+(define any-bits-set?       bitwise-any?)
+(define bit-count           bitwise-count)
+(define first-set-bit       bitwise-first-set)
+(define bit-set?            bitwise-nth?)
 
-(define (logical:reduce op4 ident)
-  (lambda args
-    (do ((res ident (op4 res (car rgs) 1 0))
-	 (rgs args (cdr rgs)))
-	((null? rgs) res))))
-
-;@
-(define (logtest n1 n2)
-  (not (zero? (logand n1 n2))))
-;@
-(define (logbit? index n)
-  (logtest (expt 2 index) n))
-;@
-(define (copy-bit index to bool)
-  (if bool
-      (logior to (arithmetic-shift 1 index))
-      (logand to (lognot (arithmetic-shift 1 index)))))
-;@
-(define (bitwise-if mask n0 n1)
-  (logior (logand mask n0)
-	  (logand (lognot mask) n1)))
-;@
-(define (bit-field n start end)
-  (logand (lognot (ash -1 (- end start)))
-	  (arithmetic-shift n (- start))))
-;@
-(define (copy-bit-field to from start end)
-  (bitwise-if (arithmetic-shift (lognot (ash -1 (- end start))) start)
-	      (arithmetic-shift from start)
-	      to))
-;@
-(define (rotate-bit-field n count start end)
-  (define width (- end start))
-  (set! count (modulo count width))
-  (let ((mask (lognot (ash -1 width))))
-    (define zn (logand mask (arithmetic-shift n (- start))))
-    (logior (arithmetic-shift
-	     (logior (logand mask (arithmetic-shift zn count))
-		     (arithmetic-shift zn (- count width)))
-	     start)
-	    (logand (lognot (ash mask start)) n))))
-;@
-(define bitwise-bit-count
-  (letrec ((logcnt (lambda (n tot)
-		     (if (zero? n)
-			 tot
-			 (logcnt (quotient n 16)
-				 (+ (vector-ref
-				     '#(0 1 1 2 1 2 2 3 1 2 2 3 2 3 3 4)
-				     (modulo n 16))
-				    tot))))))
-    (lambda (n)
-      (cond ((negative? n) (lognot (logcnt (lognot n) 0)))
-	    ((positive? n) (logcnt n 0))
-	    (else 0)))))
-;@
-(define (logcount n)
-  (cond ((negative? n) (bitwise-bit-count (lognot n)))
-	(else (bitwise-bit-count n))))
-;@
-(define (log2-binary-factors n)
-  (+ -1 (integer-length (logand n (- n)))))
-
-(define (bit-reverse k n)
-  (do ((m (if (negative? n) (lognot n) n) (arithmetic-shift m -1))
-       (k (+ -1 k) (+ -1 k))
-       (rvs 0 (logior (arithmetic-shift rvs 1) (logand 1 m))))
-      ((negative? k) (if (negative? n) (lognot rvs) rvs))))
-;@
-(define (reverse-bit-field n start end)
-  (define width (- end start))
-  (let ((mask (lognot (ash -1 width))))
-    (define zn (logand mask (arithmetic-shift n (- start))))
-    (logior (arithmetic-shift (bit-reverse width zn) start)
-	    (logand (lognot (ash mask start)) n))))
-;@
-(define (integer->list k . len)
-  (if (null? len)
-      (do ((k k (arithmetic-shift k -1))
-	   (lst '() (cons (odd? k) lst)))
-	  ((<= k 0) lst))
-      (do ((idx (+ -1 (car len)) (+ -1 idx))
-	   (k k (arithmetic-shift k -1))
-	   (lst '() (cons (odd? k) lst)))
-	  ((negative? idx) lst))))
-;@
-(define (list->integer bools)
-  (do ((bs bools (cdr bs))
-       (acc 0 (+ acc acc (if (car bs) 1 0))))
-      ((null? bs) acc)))
-(define (booleans->integer . bools)
-  (list->integer bools))
-
-;;;;@ SRFI-60 aliases
-(define ash arithmetic-shift)
-(define logior bitwise-ior)
-(define logxor bitwise-xor)
-(define logand bitwise-and)
-(define lognot bitwise-not)
-(define bit-count logcount)
-(define bit-set?   logbit?)
-(define any-bits-set? logtest)
-(define first-set-bit log2-binary-factors)
-(define bitwise-merge bitwise-if)
-;; (provide 'srfi-60)
-
-;;; Legacy
-;;(define (logical:rotate k count len) (rotate-bit-field k count 0 len))
-;;(define (logical:ones deg) (lognot (ash -1 deg)))
-;;(define integer-expt expt)		; legacy name
-
-)
+) ;module srfi-60
